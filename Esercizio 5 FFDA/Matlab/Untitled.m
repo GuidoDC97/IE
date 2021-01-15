a90f1874299aab6d780d420e90d6878a97001b95
GuidoDC97 <guidodichiara19@gmail.com>
2021-01-15 18:47:38 +0100

    Tesina completa

--- Esercizio 5 FFDA/Matlab/Untitled.m
+++ Esercizio 5 FFDA/Matlab/Untitled.m
@@ -1,5 +1,13 @@
 time = table2array(readtable('tempo_BGL_system.csv', 'HeaderLines', 1)); 
 rel = table2array(readtable('reliability_BGL_system.csv', 'HeaderLines', 1));
-%cftool(time, rel)
+cftool(time, rel)
 
-tempi_int = table2array(readtable('tempi_int.csv', 'HeaderLines', 1));
+% tempi_int = table2array(readtable('tempi_int.csv', 'HeaderLines', 1));
+% [YCDF,XCDF] = cdfcalc(tempi_int);
+% YREL = 1-YCDF;
+% 
+% cftool(XCDF, YREL);
+
+
+
+
